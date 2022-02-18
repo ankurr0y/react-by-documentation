c672ad5d590f7ebb28ace396376108bf1877918f
ankur.roy <ankur.roy@genesesolution.com>
2022-02-18 15:50:32 +0545

    Creating functions that use props and render based on these props. Use different values for functions and call as many times as you want

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Welcome from './Welcome'
 
-const name="Ankur Roy";
-const hello = <h1>Hello {name}</h1>;
+// const name="Ankur Roy";
+// const hello = <h1>Hello {name}</h1>;
 
-ReactDOM.render(
-  hello,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   hello,
+//   document.getElementById('root')
+// );
 
-function GetTime() {
-  const time_element = (
-    <div className='time'>
-      <h3>The time is {new Date().toLocaleTimeString()}</h3>
-    </div>
-  );
-  ReactDOM.render(
-    time_element,
-    document.getElementById('watch')
+// function GetTime() {
+//   const time_element = (
+//     <div className='time'>
+//       <h3>The time is {new Date().toLocaleTimeString()}</h3>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     time_element,
+//     document.getElementById('watch')
+//   );
+// }
+
+// setInterval(GetTime,1000);
+function Names(){
+  return (<>
+  <Welcome name="Ankur"></Welcome><br></br>
+  <Welcome name="Ram"></Welcome><br></br>
+  <Welcome name="Hari"></Welcome><br></br>
+  </>
   );
 }
 
-setInterval(GetTime,1000);
+ReactDOM.render(
+  <Names></Names>,
+  document.getElementById('root')
+);
