28c287b19d3d08d9f3abf346e88b29f5e83ecbcd
ankur.roy <ankur.roy@genesesolution.com>
2022-02-18 14:25:05 +0545

    Added a new ReactDOM interval that gives current time every second (A clock). Also used CSS on it to demonstrate how to apply CSS.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,17 @@ ReactDOM.render(
   hello,
   document.getElementById('root')
 );
+
+function GetTime() {
+  const time_element = (
+    <div className='time'>
+      <h3>The time is {new Date().toLocaleTimeString()}</h3>
+    </div>
+  );
+  ReactDOM.render(
+    time_element,
+    document.getElementById('watch')
+  );
+}
+
+setInterval(GetTime,1000);
